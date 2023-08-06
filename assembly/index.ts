6a87fdf02431eb2fc5aca77e051b4190c02133bb
Haru Nishiura <h24wler@gmail.com>
2023-08-06 17:21:28 +0900

    Add factorial function

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,9 @@
 // The entry file of your WebAssembly module.
 
 export function add(a: i32, b: i32): i32 {
-  return a + b;
+  return a + b
+}
+
+export function factorial(i: i32): i32 {
+  return i == 0 ? 1 : i * factorial(i - 1)
 }
--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,5 @@
-import assert from "assert";
-import { add } from "../build/debug.js";
-assert.strictEqual(add(1, 2), 3);
-console.log("ok");
+import assert from 'assert'
+import { add, factorial } from '../build/debug.js'
+assert.strictEqual(add(1, 2), 3)
+assert.strictEqual(factorial(3), 6)
+console.log('ok')
